a13bc90fe3396980e2cec5cce15892d7e2731ec3
Ron Shapiro <shapiro.rd@gmail.com>
2014-03-05 12:15:43 -0500

    Colorize the body text

--- app/src/main/java/com/venmo/lucasuses/LucasActivity.java
+++ app/src/main/java/com/venmo/lucasuses/LucasActivity.java
@@ -1,12 +1,24 @@
 package com.venmo.lucasuses;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
+import java.util.List;
 
 /**
  * {@link Activity}s are Android's concept of a "Screen". They are the Controller in the MVC pattern
@@ -52,6 +64,36 @@ public class LucasActivity extends ActionBarActivity {
         // put the TextWatcher into effect even on the android:text attribute in XML
         // For the more curious: what's another way to call this?
         bannerText.setText(bannerText.getText());
+
+        EditText bodyText = findViewExtended(R.id.meme_body_text);
+        bodyText.addTextChangedListener(new SilencingReselectingTextWatcher(bodyText) {
+            @Override
+            public void onAfterTextChanged(Editable s) {
+                List<String> lines = Lists.newLinkedList(Splitter.on('\n').split(s));
+                SpannableStringBuilder builder = new SpannableStringBuilder();
+                for (int i = 0; i < lines.size(); i++) {
+                    String line = lines.get(i);
+                    // let's insert some spans in the line
+                    Spannable span = new SpannableString(line);
+                    int color = Color.LTGRAY;
+                    if (CharMatcher.JAVA_UPPER_CASE.matchesAllOf(line)) {
+                        color = Color.BLACK;
+                    }
+                    span.setSpan(new ForegroundColorSpan(color), 0, line.length(),
+                            Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+                    builder.append(span);
+
+                    // join back '\n'; We can't use Guava's `Joiner` since it returns a String,
+                    // but we need a CharSequence (String implements CharSequence; so does
+                    // Spannable) so that the spans are transferred to the EditText
+                    if (i != lines.size() - 1) {
+                        builder.append('\n');
+                    }
+                }
+                getTextView().setText(builder);
+            }
+        });
+        bodyText.setText(bodyText.getText());
     }
 
     /**
@@ -59,7 +101,7 @@ public class LucasActivity extends ActionBarActivity {
      * result to whatever view we are assigning the returned value. Super helpful!
      */
     @SuppressWarnings("unchecked")
-    private <E extends View> E findViewExtended(int resourceId){
+    private <E extends View> E findViewExtended(int resourceId) {
         return (E) super.findViewById(resourceId);
     }
 }
---
+++ app/src/main/java/com/venmo/lucasuses/SilencingReselectingTextWatcher.java
@@ -0,0 +1,50 @@
+package com.venmo.lucasuses;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public abstract class SilencingReselectingTextWatcher implements TextWatcher {
+
+    private TextView mTextView;
+
+    public SilencingReselectingTextWatcher(TextView textView) {
+        mTextView = textView;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        // don't bother with this TextWatcher recursing by calling mTextView.setText();
+        // remove the watcher and then add it back at the end.
+        mTextView.removeTextChangedListener(this);
+        int selectionStart = mTextView.getSelectionStart();
+        int selectionEnd = mTextView.getSelectionEnd();
+
+        onAfterTextChanged(s);
+        if (mTextView instanceof EditText) {
+            EditText field = (EditText) mTextView;
+            field.setSelection(selectionStart, selectionEnd);
+        }
+
+        mTextView.addTextChangedListener(this);
+    }
+
+    public TextView getTextView() {
+        return mTextView;
+    }
+
+    /**
+     * Isolated version of {@link #afterTextChanged} - gives the {@link android.text.TextWatcher}
+     * the ability to modify the text of the {@link android.widget.TextView}.
+     *
+     * @param s see {@link #afterTextChanged(android.text.Editable)
+     */
+    public abstract void onAfterTextChanged(Editable s);
+}
